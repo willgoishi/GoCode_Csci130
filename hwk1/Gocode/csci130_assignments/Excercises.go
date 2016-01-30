7b1c9e02ff172f310692920b18d2bceb563a9f30
willgoishi <willgoishi@users.noreply.github.com>
2016-01-29 21:56:06 -0800

    Update Excercises.go

--- hwk1/Gocode/csci130_assignments/Excercises.go
+++ hwk1/Gocode/csci130_assignments/Excercises.go
@@ -10,3 +10,36 @@ func main(){
 }
 
 //3
+func main(){
+        var name string
+
+	fmt.Println("enter name:")
+
+	fmt.Scan(&name)
+
+	fmt.Println("Hello", name)
+
+}
+
+
+//4
+package main
+
+import "fmt"
+
+func main(){
+        var number_big int
+	var number_small int
+
+	fmt.Println("Enter a large number:") //asks for large number
+	fmt.Scan(&number_big)                //scans large number in
+
+	fmt.Println("Enter a small number:")
+	fmt.Scan(&number_small)
+
+	var solution int
+
+	solution = (number_big/number_small)  //division operation here and sets value to solution
+	fmt.Println("Solution is:",solution)  //prints value in solution
+
+}
