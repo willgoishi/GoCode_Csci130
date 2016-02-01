ba9b6da8f64a91e9a1daa2d936b79bea454e0c49
willgoishi <willgoishi@users.noreply.github.com>
2016-01-31 20:55:50 -0800

    Update template_conditional_statements.go

--- hwk1/Gocode/csci130_assignments/Template_using_conditional_logic/template_conditional_statements.go
+++ hwk1/Gocode/csci130_assignments/Template_using_conditional_logic/template_conditional_statements.go
@@ -1,1 +1,51 @@
 
+package main
+
+import (
+"fmt"
+"log"
+"os"
+"text/template"
+)
+
+type student struct{
+   name string
+   person_age int
+   major string
+        standing string
+        study_level string
+}
+
+func main(){
+        //temp := template.new("template.gohtml")
+
+        s:=student{
+           name : "will"
+           person_age : "20"
+                major : "csci"
+                standing : "Senior"
+        }
+
+        if s.standing == "Freshmen"{
+                s.study_level = "mild"
+        }else if s.standing == "Sophomore"{
+                s.study_level == "medium"
+
+        }else if s.standing == "junior"{
+                s.study_level = "get ready to stay up all night"
+
+        }else if s.standing == "senior"{
+                s.study_level = "eh almost done who studies!"
+        }
+
+        tpl, err := template.ParseFiles("template.gohtml")
+        if err != nil{
+                log.Fatalln(err)
+        }
+
+        err = tpl.Execute(os.Stdout, s)
+        if err != nil{
+                log.Fatalln(err)
+        }
+
+}
