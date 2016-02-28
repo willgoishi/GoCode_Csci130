a283f1cffb302f43dde4c9b8c729899d668c7620
willgoishi <willgoishi@users.noreply.github.com>
2016-02-28 08:36:43 -0800

    Update serve.go

--- hwk1/Gocode/serve_webpage/serve.go
+++ hwk1/Gocode/serve_webpage/serve.go
@@ -1,1 +1,30 @@
 
+package main
+
+import (
+        "html/template"
+        "net/http"
+	"log"
+)
+
+func serve(result http.ResponseWriter, req *http.Request){
+         t := template.New("index.gohtml")
+	 t,err := template.ParseFiles("index.gohtml")
+
+	if err != nil {
+		log.Fatal("Error:",nil)
+	}
+	t.Execute(result ,nil)
+
+}
+func main(){
+
+	http.Handle("/css/", http.StripPrefix("/css/",http.FileServer(http.Dir("css"))))
+        http.Handle("/image/", http.StripPrefix("/image/",http.FileServer(http.Dir("image"))))
+	http.HandleFunc("/",serve)
+	error := http.ListenAndServe(":9000", nil)
+
+	if error != nil{
+             log.Fatal("Error",error)
+	}
+}
