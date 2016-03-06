64aee85c263df982df5eb150886f63a176a7ccc3
willgoishi <willgoishi@users.noreply.github.com>
2016-03-05 19:32:03 -0800

    Create axwebpage.go

---
+++ hwk1/Gocode/csci130_assignments/axwebpage.go
@@ -0,0 +1,23 @@
+package main
+
+
+import (
+	      "net/http"
+        "io"
+	      "fmt"
+	/*"strings"*/
+)
+
+func main(){
+
+	http.HandleFunc("/",print_out_URL)      //(pattern string, handler func(ResponseWriter, *Request)) parameter
+	http.ListenAndServe(":8080", nil)
+}
+
+func print_out_URL(w http.ResponseWriter, req * http.Request){
+
+	//g := strings.Split(req.URL.Path,"/")
+	//gr:=g[0]
+  io.WriteString(w, "Name:" + req.URL.Path )
+	fmt.Println(req.URL.Path)
+}
