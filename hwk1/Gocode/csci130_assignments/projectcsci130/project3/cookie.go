82e1a73c9b08695a975dc694e85252680cd10a7c
willgoishi <willgoishi@users.noreply.github.com>
2016-03-10 19:17:34 -0800

    Create cookie.go

---
+++ hwk1/Gocode/csci130_assignments/projectcsci130/project3/cookie.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"log"
+	"text/template"
+	"net/http"
+	"github.com/nu7hatch/gouuid"
+	"fmt"
+)
+
+type student struct{
+	name string
+	person_age int
+	major string
+	standing string
+	study_level string
+}
+
+func serve(result http.ResponseWriter, req *http.Request){
+	temp:=template.New("project_html.html")
+
+	s:=student{
+		name : "will",
+		person_age : 20,
+		major : "csci",
+		standing : "Senior",
+	}
+
+	temp,err := temp.ParseFiles("project_html.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	temp.Execute(result, s)
+
+        //Cookie Section
+	cookie, err := req.Cookie("session")
+	if err != nil {
+		id, _ := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name:  "session",
+			Value: id.String(),
+			//Secure: true,
+			HttpOnly: true,
+		}
+		http.SetCookie(result, cookie)
+	}
+	//temp.Execute(result, nil)
+	fmt.Println(cookie)
+
+        //Form gathers user name and age and stores it into the cookie
+	name := req.FormValue("name")
+	age := req.FormValue("age")
+
+	cookie.Value = "|" + name + "|" + age
+
+	http.SetCookie(result, cookie)
+        fmt.Println()
+}
+
+
+func main(){
+
+	http.HandleFunc("/",serve)
+
+	error := http.ListenAndServe(":9000", nil)
+
+	if error != nil{
+		log.Fatal("Error",error)
+	}
+
+
+}
